03c1eebca4c5b34507f2f8338884fd2558d67013
yangjinh94259 <yangjinh94259@gmail.com>
2023-01-03 14:44:35 +0900

    test

--- com/iu/s4/objects1/ex1/Main.java
+++ com/iu/s4/objects1/ex1/Main.java
@@ -3,10 +3,14 @@ package com.iu.s4.objects1.ex1;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
+		
 		MenuController menuController = new MenuController();
 		
 		menuController.start();
 		System.out.println("집에서 추가");
+		
+		System.out.println("Finish");
 	}
 
 }
