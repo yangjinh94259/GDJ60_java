a5f59fe7f00006dc226d5d496a9bc349a67b4815
yangjinh94259 <yangjinh94259@gmail.com>
2023-01-04 09:29:25 +0900

    Goodee23/01/04

--- com/iu/s4/objects2/Car.java
+++ com/iu/s4/objects2/Car.java
@@ -2,17 +2,24 @@ package com.iu.s4.objects2;
 
 public class Car {
 
-	String company;
+	String company = "BMW";
 	String name;
 	int price;
 	String color;
 	
+	{
+		//초기화 블럭 영역
+		System.out.println("초기화 블럭 : " + this.company);
+		this.company = "AUDI";
+	}
+	
 	public Car() {
-		this("Black");		//생성자 내에서 다른 생성자 호출
-//		this.company = "KIA";
-//		this.name = "스포티지";
-//		this.price = 3500;
-//		this.color = "Black";
+//		this("Black");		//생성자 내에서 다른 생성자 호출
+		System.out.println("생성자 : " + this.company);
+		this.company = "KIA";
+		this.name = "스포티지";
+		this.price = 3500;
+		this.color = "Black";
 	}
 	
 	public Car(String color) {
--- com/iu/s4/objects2/CarMain.java
+++ com/iu/s4/objects2/CarMain.java
@@ -4,18 +4,19 @@ public class CarMain {
 
 	public static void main(String[] args) {
 		
-		
 		Car c1 = new Car();
 		c1.info();
 		
-		Car c2 = new Car("Red");
-		c2.info();
-		
-		Car c3 = new Car("Pink", 2000);
-		c3.info();
-		
-		Car c4 = new Car("Blue", 5000, "K9");
-		c4.info();
+//		c1.info();
+//		
+//		Car c2 = new Car("Red");
+//		c2.info();
+//		
+//		Car c3 = new Car("Pink", 2000);
+//		c3.info();
+//		
+//		Car c4 = new Car("Blue", 5000, "K9");
+//		c4.info();
 	}
 
 }
