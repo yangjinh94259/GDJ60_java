ee941c80ebfab232f27306da6fa9a7c5395fb35d
yangjinh94259 <yangjinh94259@gmail.com>
2023-01-05 10:53:49 +0900

    AddRobot

--- com/iu/s4/objects6/Main.java
+++ com/iu/s4/objects6/Main.java
@@ -16,6 +16,11 @@ public class Main {
 		
 		robot = null;	//객체를 소멸시키는 과정
 		
+		System.out.println("AddRobot에서 작성");
+		
+		robot = new Robot();
+		
+		System.out.println(robot);
 		System.out.println("프로그램 종료");
 	}
 
